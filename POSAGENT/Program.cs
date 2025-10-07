938aedc1b17fcac4806d0c51c1d5efd79017a0a7
NicSh <Nicolay.Shmelev@timelysoft.net>
2025-10-07 20:42:42 +0600

    Init Project

---
+++ POSAGENT/Program.cs
@@ -0,0 +1,19 @@
+namespace POSAGENT;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+            
+        builder.Services.AddWindowsService(options =>
+        {
+            options.ServiceName = "POSAGENT";
+        });
+
+        builder.Services.AddHostedService<Worker>();
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
